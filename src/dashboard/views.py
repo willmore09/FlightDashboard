4e2565f9555e8e128dea4e81b7224c77e44f2764
Matthew <matthewwillmore@me.com>
2023-05-23 21:56:57 -0700

    Really simple dashboard layout

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'dashboard/index.html')
+    context = {
+        'title': 'Flight Dashboard',
+        'alerts': ['This is a test alert'],
+        'flights': [f'Flight {i + 1}' for i in range(5)]
+    }
+    return render(request, 'dashboard/index.html', context)
--- src/flightsmonitor/settings.py
+++ src/flightsmonitor/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'dashboard.apps.DashboardConfig'
 ]
 
@@ -120,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'  # default: 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
